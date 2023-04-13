a4f8f4f21fc7dbc956ef7e141c8ed2bfeb7dcd44
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-04-13 13:56:43 +1200

    add btnClose nav

--- 3_Rotating_Navigation/script.js
+++ 3_Rotating_Navigation/script.js
@@ -22,11 +22,16 @@
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
 const container = document.querySelector(".container");
+const btnClose = document.querySelector(".close");
 
 open.addEventListener("click", () => {
   container.classList.add("show-nav");
 });
 
 close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
+
+btnClose.addEventListener("click", () => {
   container.classList.remove("show-nav");
 });
