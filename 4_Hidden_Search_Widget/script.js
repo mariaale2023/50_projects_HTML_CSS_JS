bc27ba132868dee4c76a92f768d36e59ad1c1b08
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-04-16 19:53:31 +1200

    last check

--- 4_Hidden_Search_Widget/script.js
+++ 4_Hidden_Search_Widget/script.js
@@ -4,6 +4,6 @@ const input = document.querySelector(".input");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  search.classList.toggle("active");
+  search.classList.toggle("active"); // toggle() method is used to add a class if it doesn't exist, or remove it if it does exist. Essentially, toggle() switches the state of a CSS class on or off.
   input.focus();
 });
